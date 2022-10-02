aa073bfb867235d5bfeb7c239b2e9d764548395d
Harvey <31049014+Harvest-8@users.noreply.github.com>
2022-10-02 12:22:37 +0100

    Remove sensitive config data

--- client/src/services/util/config.js
+++ client/src/services/util/config.js
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase'; //imports firbase
 
 let config = {
-    apiKey: 'AIzaSyDG13UAG9HhqQLBzaFHs18ByqQa-WZOEBo',
-    authDomain: 'boredgames-28c8e.firebaseapp.com',
-    databaseURL: 'https://boredgames-28c8e.firebaseio.com',
-    projectId: 'boredgames-28c8e',
-    storageBucket: 'boredgames-28c8e.appspot.com',
-    messagingSenderId: '501020012129',
-    appId: '1:501020012129:web:5a2e7ba131d6c5ab7d0420',
-    measurementId: 'G-52N4RGBHP9',
+    apiKey: 'api key',
+    authDomain: 'authDomain',
+    databaseURL: 'databaseURL',
+    projectId: 'projectId',
+    storageBucket: 'storageBucket',
+    messagingSenderId: 'messagingSenderId',
+    appId: 'appId',
+    measurementId: 'measurementId',
 }; //configures firebase so it can connect with the correct database
 
 firebase.initializeApp(config); //initialises firebase
